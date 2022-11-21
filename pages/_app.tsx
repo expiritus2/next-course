1a826acd5b3487ccb1bf42ce4b8619cb1aabaa50
Mikhail <michaildrazdovich@gmail.com>
2022-11-21 23:07:30 +0300

    static build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,7 @@
 import type { AppProps } from 'next/app';
-import { Layout } from 'components';
-import Head from 'next/head';
 
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <Layout>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            </Head>
-            <Component {...pageProps} />
-        </Layout>
-    )
+    return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,17 @@
-import React, { FC, Fragment } from 'react';
-import { Hero, FeaturedPosts } from 'components';
-import { GetStaticPropsResult } from 'next';
-import { getFeaturedPosts } from '../lib/posts-util';
-import { PostGrid } from '../components/posts/types';
-import Head from 'next/head';
+import React, { FC } from 'react';
 
 type ComponentProps = {
     className?: string;
-    posts: PostGrid[];
 };
 
 const HomePage: FC<ComponentProps> = (props) => {
-    const { posts } = props;
+    const {} = props;
 
     return (
-        <Fragment>
-            <Head>
-                <title>Welcome to my blog</title>
-                <meta name="description" content="I post about programming and web development."/>
-            </Head>
-            <Hero/>
-            <FeaturedPosts posts={posts}/>
-        </Fragment>
+        <div>
+            <h1>HomePage</h1>
+        </div>
     );
 };
-
-export const getStaticProps = (): GetStaticPropsResult<{ posts: PostGrid[] }> => {
-    const featuredPosts = getFeaturedPosts();
-
-    return {
-        props: {
-            posts: featuredPosts
-        }
-    }
-}
 
 export default HomePage;
